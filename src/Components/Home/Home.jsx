1e885ec67c5fecbdaad268f7227e463757c40a56
therohanmehta <rohandhn786@gmail.com>
2023-06-16 05:06:44 +0530

    all minor bug resolved

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,14 +3,7 @@ import style from "./Home.module.css";
 function Home() {
   const[number,setNumber]=useState(0)
   const data=['Programmer','Front End Web Developer','Civil Engineer','Cricketer','Guitarist','Pianist','Photgrapher']
-const bgWallpaper=['https://i.pinimg.com/736x/77/fe/96/77fe96340a9296d59acbfce203c704e2.jpg',
-'https://res.cloudinary.com/practicaldev/image/fetch/s--U5-_LOw7--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/an073jdwgkc1ztij8sj7.png',
-'https://wallpapers.com/images/hd/helmet-for-civil-engineering-eb0kjqx8al9rsjkk.jpg',
-'https://wallpaperaccess.com/full/1088580.jpg',
-'https://c4.wallpaperflare.com/wallpaper/963/490/355/guitar-strings-music-blur-wallpaper-preview.jpg ',
-'https://rare-gallery.com/thumbs/769520-Closeup-Piano.jpg',
-'https://images4.alphacoders.com/255/thumb-1920-255596.jpg'
-]
+
 
 useEffect(()=>{
   setTimeout(() => {
@@ -24,7 +17,7 @@ useEffect(()=>{
     // setNumber(0)
   }, 5000);
 
-},[number])
+})
 
   return (
     <>
